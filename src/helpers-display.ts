b3d0a1cdf506d61eb0b1a87cc5b8ef62d78c1777
Patrick Skillen <patrick.skillen2@nhs.scot>
2024-09-06 16:15:47 +0100

    chore: use a less harsh colour scheme

--- src/helpers-display.ts
+++ src/helpers-display.ts
@@ -7,7 +7,7 @@ function color(config: CardConfig, name?: string): string {
   if (!config?.color_config)
     return '';
 
-  return config.color_config[name ?? 'default'] ?? '';
+  return config.color_config[name ?? 'default_inactive'] ?? '';
 }
 
 
@@ -27,52 +27,52 @@ function renderTableHeaderRow(config: CardConfig, data: DisplayData): TemplateRe
   }
 
   return html`
-      <tr>
-          <th colspan="3">&nbsp;</th>
+    <tr>
+      <th colspan="3">&nbsp;</th>
 
-          ${(() => {
-              const groupedHeaders: TemplateResult[] = [];
-              let currentGroup: string | undefined = '';
-              let colspan = 0;
+      ${(() => {
+        const groupedHeaders: TemplateResult[] = [];
+        let currentGroup: string | undefined = '';
+        let colspan = 0;
 
-              data.columns.forEach((col, index) => {
-                  if (col.group === currentGroup) {
-                      colspan++; // Increment the colspan for the same group
-                  } else {
-                      if (colspan > 0) {
-                          groupedHeaders.push(html`<th colspan="${colspan}">${currentGroup}</th>`);
-                      }
-                      currentGroup = col.group;
-                      colspan = 1; // Start new colspan
-                  }
+        data.columns.forEach((col, index) => {
+          if (col.group === currentGroup) {
+            colspan++; // Increment the colspan for the same group
+          } else {
+            if (colspan > 0) {
+              groupedHeaders.push(html`<th colspan="${colspan}">${currentGroup}</th>`);
+            }
+            currentGroup = col.group;
+            colspan = 1; // Start new colspan
+          }
 
-                  // Handle the last column group
-                  if (index === data.columns.length - 1) {
-                      groupedHeaders.push(html`<th colspan="${colspan}">${currentGroup}</th>`);
-                  }
-              });
+          // Handle the last column group
+          if (index === data.columns.length - 1) {
+            groupedHeaders.push(html`<th colspan="${colspan}">${currentGroup}</th>`);
+          }
+        });
 
-              return groupedHeaders;
-          })()}
-          
-          <th colspan="2">Total</th>
-      </tr>
-      <tr>
-          <th>${localize("headers.time")}</th>
-          <th>${localize("headers.import")}</th>
-          <th>${localize("headers.export")}</th>
+        return groupedHeaders;
+      })()}
 
-          ${data.columns.map(col => {
-              const headerText = generateColumnHeader(col.name, col.maxPrice, col.minPrice)
+      <th colspan="2">Total</th>
+    </tr>
+    <tr>
+      <th>${localize("headers.time")}</th>
+      <th>${localize("headers.import")}</th>
+      <th>${localize("headers.export")}</th>
 
-              return html`
-                  <th>${headerText}</th>`;
-              }
-          )}
-          
-          <th>Power</th>
-          <th>Cost</th>
-      </tr>
+      ${data.columns.map(col => {
+          const headerText = generateColumnHeader(col.name, col.maxPrice, col.minPrice)
+
+          return html`
+            <th>${headerText}</th>`;
+        }
+      )}
+
+      <th>Power</th>
+      <th>Cost</th>
+    </tr>
   `
 }
 
@@ -111,11 +111,10 @@ function renderTableRow(config: CardConfig, data: DisplayData, rowNum: number): 
 
       ${row.cells.map((cell, n) => {
           const col = data.columns[n];
-          const bgColor = cell.cellActive
-            ? color(config, col.active_color)
-            : color(config, col.inactive_color);
 
+          let bgColor: string;
           let text: TemplateResult | string;
+
           if (cell.cellActive) {
             if (col.power != null && col.active_text)
               text = html`${col.active_text} ${(col.power / 1000).toFixed(config.power_decimals)} kW`;
@@ -125,12 +124,20 @@ function renderTableRow(config: CardConfig, data: DisplayData, rowNum: number): 
               text = `${(col.power / 1000).toFixed(config.power_decimals)} kW`;
             else
               text = localize('states.active');
-          } else {
+
+            bgColor = color(config, col.active_color ?? 'default_active')
+
+          } else if (cell.isActiveTime || cell.isActiveCost) {
+            // The cell might be within a timeslot which doesn't match cost, so we can show 'off' but still inactive
             if (col.inactive_text)
               text = col.inactive_text;
             else
-              // text = `iac=${isActiveCost}, iat=${isActiveTime}, colMin=${col.minPrice}, colMax=${col.maxPrice}`;
               text = localize('states.inactive');
+
+            bgColor = color(config, col.inactive_color ?? 'default_inactive');
+          } else {
+            bgColor = '';
+            text = '';
           }
 
 
@@ -139,8 +146,12 @@ function renderTableRow(config: CardConfig, data: DisplayData, rowNum: number): 
         }
       )}
 
-        <td style="color: ${color(config, row.totalPower > 0 ? 'high' : 'negative')}">${(row.totalPower/ 1000).toFixed(config.power_decimals)}kW</td>
-        <td style="color: ${color(config, row.cost > 0 ? 'high' : 'negative')}">${row.cost.toFixed(config.price_decimals)}${config.price_unit}</td>
+      <td style="color: ${color(config, row.totalPower > 0 ? 'high' : 'negative')}">
+        ${(row.totalPower / 1000).toFixed(config.power_decimals)}kW
+      </td>
+      <td style="color: ${color(config, row.cost > 0 ? 'high' : 'negative')}">
+        ${row.cost.toFixed(config.price_decimals)}${config.price_unit}
+      </td>
     </tr>`
 }
 
